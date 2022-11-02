42e8b5353ae55afabe01644dd1291ff8c643fe91
gzahn <geoff.gone@gmail.com>
2022-11-02 09:46:44 -0600

    stacked barplots

--- R/002_investigate agreement.R
+++ R/002_investigate agreement.R
@@ -2,9 +2,13 @@ library(phyloseq)
 library(vegan)
 library(tidyverse)
 library(easystats)
+library(patchwork)
+
 source("./R/plot_bar2.R")
 source("./R/palettes.R")
+
 theme_set(theme_bw())
+
 unite <- readRDS("./Data/ps_UNITE.RDS")
 euk <- readRDS("./Data/ps_UNITE_Euk.RDS")
 
@@ -38,9 +42,27 @@ ps_list_notax <- readRDS("./Data/ps_list_notax.RDS")
 # remove any faulty studies
 ps_list_notax <- ps_list_notax[which(!is.na(ps_list_notax))]
 
+# add study names to list of ps objects
+studylist <- c()
+for(i in 1:length(ps_list_notax)){
+  studylist[i] <- ps_list_notax[[i]]@sam_data$bio_project %>% unique()  
+}
+names(ps_list_notax) <- studylist
+
+
+ps_list_notax <- ps_list_notax[which(names(ps_list_notax) != "PRJNA667462")]
+
+
+
+studydata <- map(ps_list_notax,ntaxa) %>% data.frame() %>% t() %>% data.frame()
+studydata$`SRA Accession` <- row.names(studydata)
+names(studydata)[1] <- "n_taxa"
+
+
+
 # get accessions and label list elements
-names(ps_list_notax) <- 
-  ps_list_notax %>% map(sample_data) %>% map("bio_project") %>% map(unique) %>% unlist()
+# names(ps_list_notax) <- 
+#   ps_list_notax %>% map(sample_data) %>% map("bio_project") %>% map(unique) %>% unlist()
 
 # combine all studies into one ps object
 full <- 
@@ -54,7 +76,7 @@ euk <- phyloseq(otu_table(full,taxa_are_rows = FALSE),
                   tax_table(euk),
                   sample_data(full))
 
-
+rm(full)
 
 find_agreement <- function(x,y){
   agreement <- data.frame(
@@ -238,35 +260,34 @@ joined <- full_join(
   )
 )
 
-metadata <- joined %>% 
-  select(Sample,Host,Habitat) 
-metadata <- 
-metadata[seq(1,nrow(joined),by=nrow(kingdom_taxa)),] %>% unique.data.frame()
-names(metadata)[1] <- "SRA Accession"
-x <- metadata %>% 
-  arrange(Host) %>% 
-  kableExtra::kable() %>% 
-  kableExtra::kable_classic()
-saveRDS(x,"./output/manuscript_figs/metadata_table_kable.RDS")
 
 
-kingdom_mod <- 
-joined %>% 
-  filter(Rank == "Kingdom",
-         Agreement) %>% 
-  glm(data=.,
-      formula=Abundance ~ Host + Habitat)
-summary(kingdom_mod)
+# x <- metadata %>% 
+  # arrange(Host)# %>% 
+#   kableExtra::kable() %>% 
+#   kableExtra::kable_classic()
+# saveRDS(x,"./output/manuscript_figs/metadata_table_kable.RDS")
 
-kingdom_aov_mod <- 
-  joined %>% 
-  filter(Rank == "Kingdom",
-         Agreement) %>% 
-  aov(data=.,
-      formula=Abundance ~ Host + Habitat)
-summary(kingdom_aov_mod)
-HSD <- kingdom_aov_mod %>% TukeyHSD(which = "Host")
-saveRDS(kingdom_aov_mod,"./output/manuscript_figs/kingdom_aov_mod.RDS")
+
+# MODELS
+
+# kingdom_mod <- 
+# joined %>% 
+#   filter(Rank == "Kingdom",
+#          Agreement) %>% 
+#   glm(data=.,
+#       formula=Abundance ~ Host + Habitat)
+# summary(kingdom_mod)
+# 
+# kingdom_aov_mod <- 
+#   joined %>% 
+#   filter(Rank == "Kingdom",
+#          Agreement) %>% 
+#   aov(data=.,
+#       formula=Abundance ~ Host + Habitat)
+# summary(kingdom_aov_mod)
+# HSD <- kingdom_aov_mod %>% TukeyHSD(which = "Host")
+# saveRDS(kingdom_aov_mod,"./output/manuscript_figs/kingdom_aov_mod.RDS")
 
 # HSD %>% plot()
 
@@ -281,6 +302,14 @@ kingdom_taxa <- data.frame(
   UNITE = unite@tax_table[,1] %>% str_remove_all("k__"),
   UNITE_Euk = euk@tax_table[,1] %>% str_remove_all("k__")
 )
+
+metadata <- joined %>% 
+  select(Sample,Host,Habitat) 
+metadata <- 
+  metadata[seq(1,nrow(joined),by=nrow(kingdom_taxa)),] %>% unique.data.frame()
+names(metadata)[1] <- "SRA Accession"
+
+
 kingdom_taxa %>% 
   mutate(Euk_Kingdom = case_when(is.na(UNITE_Euk) ~ "unidentified",
                                  TRUE ~ UNITE_Euk),
@@ -305,4 +334,117 @@ ggsave("./output/manuscript_figs/kingdom-level_disagreement_taxonomy.png",dpi=40
 
 
 # clean up memory
-rm(list=c("unite","unite_tax","joined","euk","euk_tax","ps_list_notax","x","y","z","full"))
+rm(list=c("unite_tax","joined","euk_tax","x","y","z","full"))
+
+metadata <- 
+full_join(
+  metadata,
+  studydata
+)
+
+agreement_table <- 
+bio_project_melt %>% 
+  group_by(Sample,Rank,Agreement) %>% 
+  summarize(agreement = sum(Abundance)) %>% 
+  mutate(Rank = factor(Rank,levels = rank_names(unite))) %>% 
+  filter(!Agreement) %>% 
+  # mutate(agreement = 1-agreement) %>% 
+  # filter(Rank == "Kingdom") %>% 
+  ungroup() %>% 
+  select(Sample,Rank,agreement) %>% 
+  rename("SRA Accession"="Sample",
+         "Proportion_Disagreement" = "agreement") %>% 
+  full_join(metadata) %>% 
+  mutate(Proportion_Disagreement = 1-Proportion_Disagreement) %>% 
+  rename("Proportion_Agreement" = "Proportion_Disagreement") %>% 
+  pivot_wider(names_from = Rank,values_from = Proportion_Agreement) %>% 
+  select(`SRA Accession`,Habitat,Host,n_taxa,Kingdom,Phylum,Class,Order,Family,Genus,Species) %>% 
+  arrange(Host)
+
+agreement_matrix <- 
+agreement_table %>% 
+  select(Kingdom,Phylum,Class,Order,Family,Genus,Species) %>% 
+  as.matrix
+
+# heatmap(agreement_matrix,Rowv = NA,Colv = NA,ColSideColors = )
+
+long_agreement <- 
+agreement_table %>% 
+  pivot_longer(all_of(c("Kingdom","Phylum","Class","Order","Family","Genus","Species")),
+               names_to = "Rank", values_to = "Agreement")
+
+text_color <-   
+agreement_table %>% 
+  mutate(text_color = case_when(Host == "Aerobiota" ~ "Purple",
+                                Host == "Animal" ~ "DarkRed",
+                                Host == "Aquatic" ~ "Blue",
+                                Host == "Plant" ~ "Dark Green",
+                                Host == "Soil" ~ "Black")) %>% 
+  pluck("text_color")
+
+metadata
+shannon <- function(x){
+  y <- estimate_richness(x,measures = 'Shannon')
+  y$Shannon %>% mean(na.rm=TRUE)
+}
+
+shannon_list <- ps_list_notax %>% map(shannon)
+shannon <- as.data.frame(shannon_list) %>% t() %>% data.frame()
+shannon$`SRA Accession` <- row.names(shannon)
+names(shannon)[1] <- "Shannon div"
+
+metadata <- 
+metadata %>% 
+  full_join(shannon)
+
+
+
+hm <- 
+long_agreement %>%
+  mutate(Rank=Rank %>% factor(levels = rank_names(host_ps))) %>% 
+  ggplot(aes(Rank,`SRA Accession`,fill=Agreement)) +
+  geom_tile() +
+  scale_fill_viridis_c(begin = .1,end = .8, option = 'cividis') +
+  theme(axis.text.x = element_text(angle=60,hjust = 1,face='bold'),
+        axis.text.y = element_text(color=text_color,face='bold'),
+        axis.title = element_text(face='bold',size=16),
+        legend.title = element_text(face='bold',size=12),
+        legend.text = element_text(face='bold')) +
+  labs(y="SRA Accession\n\n\n\n",x="\nRank")
+ggsave("./output/manuscript_figs/project_agreement_heatmap.png",dpi=400,width = 4,height = 6)
+
+saveRDS(hm,"./output/manuscript_figs/agreement_by_bioproject_barplot.RDS")
+
+unite@tax_table[,2] <- unite@tax_table[,2] %>% str_remove("p__") %>% str_remove("Fungi_phy_")
+euk@tax_table[,2] <- euk@tax_table[,2] %>% str_remove("p__") %>% str_remove("Fungi_phy_")
+
+unite_barplot <- 
+unite %>% 
+  subset_taxa(Kingdom == "k__Fungi") %>% 
+  merge_samples("bio_project") %>% 
+  transform_sample_counts(function(x)x/sum(x)) %>% 
+  plot_bar2(fill = "Phylum") +
+  labs(y="Relative abundance",x="Study accession",title = "UNITE Fungi") + 
+  # theme(legend.position = 'none') +
+  scale_fill_manual(values = pal.discrete)
+
+
+euk_barplot <- 
+euk %>% 
+  subset_taxa(Kingdom == "k__Fungi") %>% 
+  merge_samples("bio_project") %>% 
+  transform_sample_counts(function(x)x/sum(x)) %>% 
+  plot_bar2(fill = "Phylum") +
+  labs(y="Relative abundance",x="Study accession",title = "UNITE All") + 
+  # theme(legend.position = 'bottom') +
+  scale_fill_manual(values = pal.discrete)
+
+
+
+patch <- unite_barplot / euk_barplot
+patch + plot_layout(guides = 'collect') &
+  theme(legend.position = 'bottom')
+ggsave("./output/manuscript_figs/mixed_barplot_by_study.png",height = 8,width = 18,dpi = 400)
+
+
+metadata
